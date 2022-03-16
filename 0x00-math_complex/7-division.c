d2027cd411135f8b007b7aa6b46bb9c1bc38a242
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-03-16 08:38:06 +0200

    third commit

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -1,23 +1,19 @@
 #include "holberton.h"
-#include <math.h>
-
 /**
-* division - return the division
-* @c1: complex
-* @c2: complex
-* @c3: complex pointer
-* Return: void
-*/
-
+ * division - function that performs the division operation to complex numbers
+ *
+ * @c1: Parameter structure 1
+ * @c2: Parameter structure 2
+ * @c3: Parameter pointer to structure
+ *
+ * Return: return the division
+ */
 void division(complex c1, complex c2, complex *c3)
 {
-/* all at once */
-double a = c1.re;
-double b = c1.im;
+	c3->re = ((c1.re * c2.re) + (c1.im * c2.im));
+	c3->re /= ((c2.re * c2.re) + (c2.im * c2.im));
 
-double c = c2.re;
-double d = c2.im;
-/* to structure */
-c3->re = ((a * c) + (b * d)) / (pow(c, 2) + pow(d, 2));
-c3->im = ((b * c) - (a * d)) / (pow(c, 2) + pow(d, 2));
+	c3->im = ((c1.re * -c2.im) + (c1.im * c2.re));
+	c3->im /= ((c2.re * c2.re) + (c2.im * c2.im));
 }
+
