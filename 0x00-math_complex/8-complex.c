4d1badb8c6fd205b2bc59980b7f0f395000a8875
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-03-16 08:39:26 +0200

    third commit

--- 0x00-math_complex/8-complex.c
+++ 0x00-math_complex/8-complex.c
@@ -1,31 +1,17 @@
 #include "holberton.h"
-#include <math.h>
-
 /**
-* complex_from_mod_arg - update complex
-* @m: double
-* @arg: double
-* @c3: complex
-* Return: void
-*/
-
+ * complex_from_mod_arg - function that update the real and the
+ * imaginary parts of
+ * a complex number given its modulus and arguments.
+ *
+ * @m: Parameter
+ * @arg: Parameter
+ * @c3: Parameter pointer to structure
+ *
+ * Return: return the multiplication
+ */
 void complex_from_mod_arg(double m, double arg, complex *c3)
 {
-/* take distance from modulus */
-/* take angle from argument */
-/* senØ = a/c; c = Hypotenuse a = oposite catetus  */
-/* cosØ = b/c; c = Hypotenuse b = adyacent catetus */
-/* tanØ = a/b;  */
-
-/* apply pitagoras to get the values */
-/* im part */
-double a;
-/* re part */
-double b;
-
-a = sin(arg) * m;
-b = cos(arg) * m;
-
-c3->re = b;
-c3->im = a;
+	c3->re = m * cos(arg);
+	c3->im = m * sin(arg);
 }
