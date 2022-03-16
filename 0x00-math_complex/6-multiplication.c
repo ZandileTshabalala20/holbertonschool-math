0a1d0e5a96bf46ce38274acd652b071bc2e5578c
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-03-16 08:36:06 +0200

    third commit

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -1,14 +1,12 @@
 #include "holberton.h"
-#include <math.h>
-
 /**
-* multiplication - return the multiplication
-* @c1: complex
-* @c2: complex
-* @c3: complex pointer
-* Return: void
-*/
-
+ * multiplication - function that performs the multiplication
+ * operation to complex numbers
+ *
+ * @c1: Parameter structure 1
+ * @c2: Parameter structure 2
+ * @c3: Parameter pointer to structure
+ */
 void multiplication(complex c1, complex c2, complex *c3)
 {
 	c3->re = (c1.re * c2.re) - (c1.im * c2.im);
