a8c32382f9957256564d1274b394911c3097adde
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2022-03-16 07:31:02 +0200

    third commit

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -1,18 +1,24 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 #include <stdio.h>
-#include <stdarg.h>
+#include <math.h>
 
 /**
-* struct complex - Structure
-*/
-
-typedef struct complex
+ * struct complex_number - Struct with the elements
+ *
+ * @re: real member
+ * @im: imaginary member
+ *
+ * Description: structure with the imaginary number
+ * members
+ */
+struct complex_number
 {
 	double re;
 	double im;
-} complex;
+};
+typedef struct complex_number complex;
 
 void display_complex_number(complex c);
 complex conjugate(complex c);
@@ -22,6 +28,6 @@ void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
-void complex_from_mod_arg(double m, double arg, complex *c3);
+void complex_from_mod_arg (double m, double arg, complex *c3);
 
 #endif
